10080b42ad40e7ec8e787e68f165a70a6eb0d8ed
Mukiria <85222480+Mukiria@users.noreply.github.com>
2026-07-31 04:27:33 +0300

    Update sudoku_engine.dart

--- lib/features/sudoku/engine/sudoku_engine.dart
+++ lib/features/sudoku/engine/sudoku_engine.dart
@@ -1,5 +1,5 @@
-export 'models/board.dart';
+export 'generator/puzzle_generator.dart';
 export 'models/cell.dart';
+export 'models/board.dart';
 export 'solver/sudoku_solver.dart';
 export 'validator/unique_solution_validator.dart';
-export 'generator/puzzle_generator.dart';
